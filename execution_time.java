7a32f3f3693d059a7113606717f8c3bcaeb5d79f
Divya <mittald778@gmail.com>
2024-08-20 23:45:51 +0530

    java exec time

---
+++ execution_time.java
@@ -0,0 +1,22 @@
+import java.util.*;
+    class Main {
+        public void display() {
+          System.out.println("Calculating Method execution time:");
+        }
+      
+        public static void main(String[] args) {
+      
+          Main obj = new Main();
+      
+          long start = System.nanoTime();
+      
+          obj.display();
+      
+          long end = System.nanoTime();
+    
+          long execution = end - start;
+          System.out.println("Execution time: " + execution + " nanoseconds");
+        }
+      }
+    
+
